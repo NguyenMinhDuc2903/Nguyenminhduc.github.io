42f32f1f74483c893db301b433af680354037a6c
unknown <md591ad@gmail.com>
2022-08-26 11:16:44 +0700

    Foler upload

---
+++ git.vb
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title> Wed ca nhan cua DUck </title>
+    </head>
+<body>
+    <h1> Nguyen Minh Duck </h1>
+    <div class="Information">
+        <b> Tên:</b> <i> Nguyễn Minh Đức </i>
+    </div>
+    <div   class="Link Fb "> 
+        <i> <a href="www.facebook.com/md590ad"> www.facebook.com/md590ad</a></i>
+    </div>
+    </body>
+</html>
